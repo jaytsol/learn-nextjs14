1424082beb5ddadb62fbf777404a6a35e890b464
jaytsol <jaytsol@naver.com>
2025-02-16 01:57:36 +0900

    fixed IParams

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,16 +2,14 @@ import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
+interface IParams {
+    params: { id: string };
+  }
+
 export async function generateMetadata({ params: { id }}: IParams) {
   const movie = await getMovie(id);
   return {
     title: movie.title,
-  }
-}
-
-interface IParams {
-  params: {
-    id: string;
   }
 }
 
