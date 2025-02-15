845b54c875b86d805de3aea3f0ae2caa235c73d1
jaytsol <jaytsol@naver.com>
2025-02-15 18:39:38 +0900

    Homepage css fixed

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,10 +1,8 @@
 import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string) {
-    await new Promise(resolve => setTimeout(resolve, 3000));
-    throw new Error('Failed to fetch videos');
-    // const res = await fetch(`${API_URL}/${id}/videos`);
-    // return res.json();
+    const res = await fetch(`${API_URL}/${id}/videos`);
+    return res.json();
   }
 
 export default async function MovieVideos({ id }: { id: string }) {
